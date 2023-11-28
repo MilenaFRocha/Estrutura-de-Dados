a651b8ab0bf40a0ae8ebfca115d012e4d7b63f84
Milena Linda <milenafernandes594@gmail.com>
2023-11-28 00:20:10 -0300

    finalizado os algortmos de busca

--- Algoritmos de busca/BuscaSequencial_Ordenado.c
+++ Algoritmos de busca/BuscaSequencial_Ordenado.c
@@ -12,29 +12,6 @@ int buscaSequencial_Ordenado( int * vetor, int tamanho, int chave){
     }
 }
 
-int BuscaBinaria_Ordenado(int * vet, int tamanho , int chave){
-
-    int inicio =0;
-    int fim = tamanho -1;
-
-    while (inicio <= fim){
-
-        int meio = (inicio + fim)/2;
-        if (vet[meio]== chave){
-            return meio;
-            break;
-        }
-        if (vet[meio] > chave){
-            fim = meio -1;
-
-        }
-        {
-            inicio = meio +1;
-        
-        }
-    }
-    
-}
 
 
 int main(void){
@@ -42,6 +19,5 @@ int main(void){
      int vet[] = {1,2,3,4,4};
      printf("%d", buscaSequencial_Ordenado(vet ,5,4));
      
-    printf("%d",BuscaBinaria_Ordenado(vet ,5,4));
     return 0;
 }
